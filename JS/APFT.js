e606895eb324f799a3a04bef0105ebca939b8a35
queenskid <andres.ibarra0716@gmail.com>
2017-10-09 16:57:57 -0700

    constructor started with minor inputs

--- JS/APFT.js
+++ JS/APFT.js
@@ -1,1 +1,22 @@
 'use strict'
+
+var ServiceMember.allMembers = [];
+var maleData = [];
+var femaleData = [];
+
+
+function ServiceMember(name, age, gender) {
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+  this.height =
+  this.weight = 
+  this.maleData = 0;
+  this.femaleData = 0;
+  ServiceMember.allMembers.push(this);
+
+};
+
+new ServiceMember('Kerry', '32', true);
+new ServiceMember('Ariel', '31', true);
+new ServiceMember('Dre', '34', true);
