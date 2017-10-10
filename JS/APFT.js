0758a6888fd3aac96151c5c834e575db218535a9
queenskid <andres.ibarra0716@gmail.com>
2017-10-09 17:40:19 -0700

    changes to constructor

--- JS/APFT.js
+++ JS/APFT.js
@@ -1,21 +1,27 @@
-'use strict'
+'use strict';
 
 var ServiceMember.allMembers = [];
-var maleData = [];
-var femaleData = [];
-
 
 function ServiceMember(name, age, gender) {
   this.name = name;
   this.age = age;
   this.gender = gender;
-  this.height =
-  this.weight = 
-  this.maleData = 0;
-  this.femaleData = 0;
+  this.height = height;
+  this.weight = weight;
+  this.isTreshold = false;
+  // this.maleData = 0;
+  // this.femaleData = 0;
+  this.testHistory = [][];
+  this.neck;
+  this.waist;
+  this.hips;
   ServiceMember.allMembers.push(this);
 
 };
+
+ServiceMember.prototype.checkIfFat() {
+  
+}
 
 new ServiceMember('Kerry', '32', true);
 new ServiceMember('Ariel', '31', true);
