bca694b3ffa17cd77b699ab372574a7db1accc31
Ariel R. Pedraza <pedrazaa2@gmail.com>
2017-10-10 14:09:20 -0700

    started first fitness check

--- JS/APFT.js
+++ JS/APFT.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var formEl = document.getElementById('user-input');
-var currentUserIndex;
+var currentUserIndex = 0;
 ServiceMember.allMembers = [];
 
 function ServiceMember(name, gender, age) {
@@ -19,7 +19,24 @@ function ServiceMember(name, gender, age) {
 }
 
 ServiceMember.prototype.checkIfFat = function() {
-  //
+  //check age to determine
+  var i;
+  if (this.age >= 17 || this.age <= 20) {
+    i = 0;
+  } else if (this.age >= 21 || this.age <= 27) {
+    i = 1;
+  } else if (this.age >= 28 || this.age <= 39) {
+    i = 2;
+  } else if (this.age >= 40) {
+    i = 3;
+  } else {
+    console.log('age is below 17');
+  }
+  //check gender to determine which array to use
+  var j = heightRange.indexOf(Math.ceil(parseInt(this.height)));
+  console.log('index i: ' + i);
+  console.log(Math.ceil(this.height));
+  console.log('index j: ' + j);
 };
 
 ServiceMember.prototype.getAge = function(dateString) {
