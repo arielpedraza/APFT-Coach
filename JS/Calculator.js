658334d2ea5f460f0a9c0250fd036a8cc56a7d7f
queenskid <andres.ibarra0716@gmail.com>
2017-10-11 12:00:44 -0700

    calculator functions started

--- JS/Calculator.js
+++ JS/Calculator.js
@@ -3,13 +3,40 @@
 function ServiceMember(gender, age) {
   this.gender = gender;
   this.age = age;
-  this.ageRange = 2;
+  this.ageRange = '';
   this.pushUps = 0;
   this.sitUps = 0;
   this.runTime = 0;
 };
 
+var pushUpsEntry = document.getElementById('pushUps');
+var sitUpsEntry = document.getElementById('sitUps');
+var runTimeEntry = document.getElementById('runTime');
+
 console.log(apftTestResults.male['push-ups']['5'][0]['22-26']);
 
+function handlePushUps(e) {
+  e.preventDefault();
 
-// if(this)
+  var x = ServiceMember.allMembers[0].pushUps;
+  var pushUps = parseInt(e.target.pushUps.value);
+  x = pushUps;
+};
+
+function handleSitUps(e) {
+  e.preventDefault();
+  var y = ServiceMember.allMembers[0].sitUps;
+  var sitUps = parseInt(e.target.sitUps.value);
+  y = sitUps;
+};
+
+function handleRunTime(e) {
+  e.preventDefault();
+  var z = ServiceMember.allMembers[0].runTime;
+  var runTime = parseInt(e.target.runTime.value);
+  z = runTime;
+};
+
+pushUpsEntry.addEventListener('input', handlePushUps);
+sitUpsEntry.addEventListener('input', handleSitUps);
+runTimeEntry.addEventListener('input', handleRunTime);
