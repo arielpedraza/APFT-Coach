e7b18a54ec3e276c42bdc4c88c953d0b9fb7497e
queenskid <andres.ibarra0716@gmail.com>
2017-10-11 11:17:20 -0700

    js calculator started

--- JS/Calculator.js
+++ JS/Calculator.js
@@ -1,1 +1,15 @@
-'use strict'
+'use strict';
+
+function ServiceMember(gender, age) {
+  this.gender = gender;
+  this.age = age;
+  this.ageRange = 2;
+  this.pushUps = 0;
+  this.sitUps = 0;
+  this.runTime = 0;
+};
+
+console.log(apftTestResults.male['push-ups']['5'][0]['22-26']);
+
+
+// if(this)
