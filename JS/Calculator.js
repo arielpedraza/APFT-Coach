23ba63b400ba20cb0d1fbf4253fa0eb1247b206a
Ariel R. Pedraza <pedrazaa2@gmail.com>
2017-10-12 11:57:51 -0700

    input validation

--- JS/Calculator.js
+++ JS/Calculator.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var pEl;
 ServiceMember.allMembers = [];
 
 function ServiceMember(name, gender, age, height, weight) {
@@ -23,11 +24,13 @@ function ServiceMember(name, gender, age, height, weight) {
   ServiceMember.allMembers.push(this);
 }
 
-
 //Parsed information from first page
 ServiceMember.allMembers = JSON.parse(localStorage.getItem('allMembers'));
 
 ServiceMember.allMembers[0].totalScore = 0;
+ServiceMember.allMembers[0].runScore = 0;
+ServiceMember.allMembers[0].sitUpsScore = 0;
+ServiceMember.allMembers[0].pushUpsScore = 0;
 
 //Get scores by cross referencing user input with JSON object data
 function getScores(a) {
@@ -35,52 +38,74 @@ function getScores(a) {
   //Each event handler calls different getScores() by hard coding arguments 1, 2, or 3
   //if called by push up event handler
   if (a === 1) {
-    x = apftTestResults[ServiceMember.allMembers[0].genderStr]['push-ups'][ServiceMember.allMembers[0].pushUps][0][ServiceMember.allMembers[0].ageRange];
-    return x;
+    if (ServiceMember.allMembers[0].pushUps < 5) {
+      x = 0;
+      ServiceMember.allMembers[0].pushUpsScore = x;
+    } else if (ServiceMember.allMembers[0].pushUps > 77) {
+      x = 100;
+      ServiceMember.allMembers[0].pushUpsScore = x;
+    } else {
+      x = apftTestResults[ServiceMember.allMembers[0].genderStr]['push-ups'][ServiceMember.allMembers[0].pushUps][0][ServiceMember.allMembers[0].ageRange];
+      ServiceMember.allMembers[0].pushUpsScore = x;
+    }
+    pEl = document.getElementById('pushUpScore');
+    pEl.textContent = x;
+    updateTotalScore();
   }
   // if called by sit up event handler
   if (a === 2) {
-    y = apftTestResults[ServiceMember.allMembers[0].genderStr]['sit-ups'][ServiceMember.allMembers[0].sitUps][0][ServiceMember.allMembers[0].ageRange];
-    return y;
+    if (ServiceMember.allMembers[0].sitUps < 21) {
+      y = 0;
+      ServiceMember.allMembers[0].sitUpsScore = y;
+    } else if (ServiceMember.allMembers[0].sitUps > 82) {
+      y = 100;
+      ServiceMember.allMembers[0].sitUpsScore = y;
+    } else {
+      y = apftTestResults[ServiceMember.allMembers[0].genderStr]['sit-ups'][ServiceMember.allMembers[0].sitUps][0][ServiceMember.allMembers[0].ageRange];
+      ServiceMember.allMembers[0].sitUpsScore = y;
+    }
+    pEl = document.getElementById('sitUpScore');
+    pEl.textContent = y;
+    updateTotalScore();
   }
-  //Used modulo to induce 6 sec intervals to second user input so that it would match JSON run data
   // if called by run second handler
   if (a === 3) {
+    //Used modulo to induce 6 sec intervals to second user input so that it would match JSON run data
     var temp = parseInt(ServiceMember.allMembers[0].runTimeSec);
-    if (temp % 6 ) {
+    if (temp % 6) {
       temp = 6 - (temp % 6) + temp;
-  //adds difference back to seconds if not a 6 sec interval
+      //adds difference back to seconds if not a 6 sec interval
       z = ServiceMember.allMembers[0].runTimeMin + '' + temp;
     } else {
-    z = ServiceMember.allMembers[0].runTime;
+      ServiceMember.allMembers[0].runTime = runTimeMin.value + '' + runTimeSec.value;
+      z = ServiceMember.allMembers[0].runTime;
     }
-    z = apftTestResults[ServiceMember.allMembers[0].genderStr]['run'][z][0][ServiceMember.allMembers[0].ageRange];
-    return z;
+    //assign points if outside table bounds.
+    if (parseInt(z) < 1254) {
+      z = 100;
+      ServiceMember.allMembers[0].runScore = z;
+    } else if (parseInt(z) > 2630) {
+      z = 0;
+      ServiceMember.allMembers[0].runScore = z;
+    } else {
+      z = apftTestResults[ServiceMember.allMembers[0].genderStr]['run'][z][0][ServiceMember.allMembers[0].ageRange];
+      ServiceMember.allMembers[0].runScore = z;
+    }
   }
-};
-//adds score from JSON data to empty p tags in HTML
-function printScores(a, x) {
-  var pEl4 = document.getElementById('totalScore');
-    if (a === 1) {
-    var pEl1 = document.getElementById('pushUpScore');
-    pEl1.textContent = x;
-    ServiceMember.allMembers[0].totalScore += x;
-    pEl4.textContent = ServiceMember.allMembers[0].totalScore;
-  }
-    if (a === 2) {
-      var pEl2 = document.getElementById('sitUpScore');
-      pEl2.textContent = x;
-      ServiceMember.allMembers[0].totalScore += x;
-      pEl4.textContent = ServiceMember.allMembers[0].totalScore;
+  pEl = document.getElementById('runTimeScore');
+  pEl.textContent = z;
+  updateTotalScore();
+}
 
-  }
-    if (a === 3) {
-    var pEl3 = document.getElementById('runTimeScore');
-    pEl3.textContent = x;
-    ServiceMember.allMembers[0].totalScore += x;
-    pEl4.textContent = ServiceMember.allMembers[0].totalScore;
-  }
-};
+function updateTotalScore() {
+  pEl = document.getElementById('totalScore');
+  console.log(ServiceMember.allMembers[0].pushUpsScore);
+  console.log(ServiceMember.allMembers[0].sitUpsScore);
+  console.log(ServiceMember.allMembers[0].runScore);
+  console.log(ServiceMember.allMembers[0].totalScore);
+  ServiceMember.allMembers[0].totalScore = ServiceMember.allMembers[0].pushUpsScore + ServiceMember.allMembers[0].sitUpsScore + ServiceMember.allMembers[0].runScore;
+  pEl.textContent = ServiceMember.allMembers[0].totalScore;
+}
 
 var pushUpsEntry = document.getElementById('pushUps');
 var sitUpsEntry = document.getElementById('sitUps');
@@ -90,37 +115,35 @@ var runTimeSec = document.getElementById('runTimeSec');
 // console.log(apftTestResults.male['push-ups']['5'][0]['22-26']);
 
 function handlePushUps(event) {
-  event.preventDefault();
-  console.log(pushUpsEntry.value);
-  ServiceMember.allMembers[0].pushUps = parseInt(pushUpsEntry.value);
-  var score = getScores(1);
-  console.log(score);
-  printScores(1, score);
+  if (pushUpsEntry.value){
+    event.preventDefault();
+    ServiceMember.allMembers[0].pushUps = parseInt(pushUpsEntry.value);
+    getScores(1);
+  }
 };
 
 function handleSitUps(event) {
-  event.preventDefault();
-  console.log(sitUpsEntry.value);
-  console.log(ServiceMember.allMembers[0]);
-  ServiceMember.allMembers[0].sitUps = parseInt(sitUpsEntry.value);
-  var score = getScores(2);
-  console.log(score);
-  printScores(2, score);
+  if (sitUpsEntry.value){
+    event.preventDefault();
+    ServiceMember.allMembers[0].sitUps = parseInt(sitUpsEntry.value);
+    getScores(2);
+  }
 };
 
 function handleRunMin(event) {
-  event.preventDefault();
-  ServiceMember.allMembers[0].runTimeMin = runTimeMin.value;
-  console.log(ServiceMember.allMembers[0].runTime)
+  if (runTimeMin.value) {
+    event.preventDefault();
+    ServiceMember.allMembers[0].runTimeMin = runTimeMin.value;
+  }
 };
 
 function handleRunSec(event) {
-  event.preventDefault();
-  ServiceMember.allMembers[0].runTimeSec = runTimeSec.value;
-  ServiceMember.allMembers[0].runTime = runTimeMin.value + '' + runTimeSec.value
-  var score = getScores(3);
-  console.log(ServiceMember.allMembers[0].runTime);
-  printScores(3, score);
+  if (runTimeSec.value) {
+    event.preventDefault();
+    ServiceMember.allMembers[0].runTimeSec = runTimeSec.value;
+    // ServiceMember.allMembers[0].runTime = runTimeMin.value + '' + runTimeSec.value;
+    getScores(3);
+  }
 };
 
 pushUpsEntry.onblur = handlePushUps;
